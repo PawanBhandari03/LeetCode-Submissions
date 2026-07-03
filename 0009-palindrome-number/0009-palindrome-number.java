7018e446f596a124924167fa63a090ff131e972e
PawanCodes <pawansinghb07@gmail.com>
2026-07-03 20:39:27 +0530

    Time: 4 ms (100%), Space: 46 MB (53.16%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if(x < 0)
+            return false;
+
+        int ogno = x;
+        int revno = 0;
+
+        while(x > 0) {
+            int lastdigit = x % 10;
+            revno = revno * 10 + lastdigit;
+            x = x / 10;
+        }
+        return revno == ogno;
+    }
+}
